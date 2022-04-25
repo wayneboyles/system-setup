43e2b59acfc6b3f876d71992e98d5f9bce6f41cf
Wayne <wayne_boyles@hotmail.com>
2022-04-25 14:55:22 -0500

    Update vim.sh

--- linux/TEMP/components/vim.sh
+++ linux/TEMP/components/vim.sh
@@ -3,6 +3,6 @@
 logInfo "Configuring the vim editor..."
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs --silent https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-curl -fLo ~/.vimrc --silent https://raw.githubusercontent.com/wayneboyles/system-setup/main/linux/resources/.vimrc
+curl -fLo ~/.vimrc --silent https://raw.githubusercontent.com/wayneboyles/system-setup/main/linux/TEMP/resources/.vimrc
 
 vim -E -s -u ~/.vimrc +PlugInstall +qall
